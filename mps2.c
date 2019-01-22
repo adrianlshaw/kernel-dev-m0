8508c5859398bdafad93f09c66815672796b5366
Adrian L. Shaw <adrianlshaw@gmail.com>
2019-01-22 22:21:39 +0000

    Added UART support for MPS2

--- mps2.c
+++ mps2.c
@@ -1,14 +1,18 @@
 #define UART_BASE 0x40200000 // to 0x40200FFF
-#define UARTDR (UART_BASE + 0x0)
-#define UARTRSR (UART_BASE + 0x4)
+#define UART_CTRL (UART_BASE + 0x8)
+#define UART_BAUDDIV (UART_BASE + 0x010)
 
 void uart_init()
 {
+	volatile unsigned long *baud = UART_BAUDDIV;
+	volatile unsigned long *ctrl = UART_CTRL;
+	*baud = 16;
+	*ctrl = 1; // Enable
 }
 
 void putc(void *p, const char c)
 {
-	iowrite32(UARTDR, (unsigned int) c);
+	iowrite32(UART_BASE, (unsigned int) c);
 }
 
 void enable_timer(void) {
